c12cd0061ba153426614fd00c9ff2282d373c23f
MosammatHabibaRahman <habiba9g@yahoo.com>
2020-12-31 17:38:27 +0600

    Initial Template

--- E-CommerceSystemWithRestAPI/Models/WishlistItem.cs
+++ E-CommerceSystemWithRestAPI/Models/WishlistItem.cs
@@ -12,6 +12,7 @@ namespace E_CommerceSystemWithRestAPI.Models
         [Required]
         public int ProductId { get; set; }
         public Product Product { get; set; }
+        public int WishlistId { get; set; }
         public Wishlist Wishlist { get; set; }
     }
 }
